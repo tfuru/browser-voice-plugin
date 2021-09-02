93a00c7863105762e68d2c7d1cb6504945b18e56
Chenxi-Liu <329052927@qq.com>
2021-09-02 20:15:39 +0800

    update: entry

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // 通过环境变量来决定入口文件
 if (process.env.NODE_ENV === 'production') {
-  module.exports = require('./dist/speechJs.min.js')
+  module.exports = require('./dist/voicePlugin.min.js')
 } else {
-  module.exports = require('./dist/speechJs.js')
+  module.exports = require('./dist/voicePlugin.js')
 }
